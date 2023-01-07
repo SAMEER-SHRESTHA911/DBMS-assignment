73ab3c5678953a053bfee7f4a6be985fef45a3f4
S A M E E R <59270895+SAMEER-SHRESTHA911@users.noreply.github.com>
2023-01-07 19:08:57 +0545

    Add files via upload

---
+++ database_creation_for_assignment_1.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE db_employee;
+
+USE db_employee;
+
+CREATE TABLE employee (
+	employee_name varchar(255) PRIMARY KEY,
+	street varchar(255),
+	city varchar(255)
+);
+
+INSERT INTO employee(employee_name, street, city)
+VALUES('James Arthur', 'Street 7', 'Middlesbrough'),
+	  ('James Hillier Blount', 'Tidworth', 'Hampshire'),
+      ('James Corden', 'Street 420' ,'Hillingdon'),
+      ('James Alan Hetfield','Downey', 'California'),
+	  ('Sushank Ghimire','Bouddha','Kathmandu'),
+      ('Arahanta POkhrel','Buddhanagar','Kathmandu'),
+      ('Sudeep Kaucha','Buddhanagar','Kathmandu'),
+      ('Bikrant Bidari','Patan','Lalitpur'),
+      ('Sujan Lamichane','Bhaisepati','Lalitpur'),
+      ('Rajin Khatri','Thimi','Bhaktapur'),
+      ('Pilot Khadka','Pilotbaba', 'Hampshire');
+
+-- DROP TABLE employee; 
+ 
+SELECT * FROM employee;
+
+CREATE TABLE works( 
+	employee_name varchar(255)PRIMARY KEY,
+    company_name varchar(255),
+    salary integer
+);
+
+INSERT INTO works(employee_name, company_name, salary)
+VALUES('James Arthur','Recovery', 39000),
+	  ('James Hillier Blount','Superstar', '46000'),
+      ('James Corden','Central', '2000'),
+      ('James Alan Hetfield','Small Bank Corporation','78000'),
+      ('Sushank Ghimire','First Bank Corporation','12000'),
+      ('Arahanta Pokhrel','First Bank Corporation',5000),
+      ('Bikrant Bidari','Recovery',60000),
+      ('Rajin Khatri','Central',50000),
+      ('Sujan Lamichane','Small Bank Corporation',45000),
+      ('Sudeep Kaucha','First Bank Corporation',20000),
+      ('Pilot Khadka', 'Superstar',30000);
+      
+-- DROP TABLE works;
+
+SELECT * FROM works;
+
+CREATE TABLE company(
+	company_name varchar(255) PRIMARY KEY,
+    city varchar(255)
+);
+
+INSERT INTO company(company_name, city)
+VALUES('Recovery','Hampshire'),
+	  ('Superstar','Yorkshire'),
+      ('Central','New York'),
+      ('Small Bank Coropration','California'),
+      ('First Bank Corporation','Kathmandu');
+      
+-- DROP TABLE company;
+
+SELECT * FROM company;
+
+CREATE TABLE manages(
+	employee_name varchar(255) PRIMARY KEY,
+    manager_name varchar(255)
+);
+
+INSERT INTO manages(employee_name, manager_name)
+VALUES('James Arthur', 'Bikrant Bidari'),
+	  ('James Hillier Blount','Pilot Khadka'),
+      ('James Corden','Rajin Khatri'),
+      ('James Alan Hetfield','Sujan Lamichane'),
+      ('Sushank Ghimire','Sudeep Kaucha'),
+      ('Arahanta Pokhrel','Sudeep Kaucha');
+      
+-- DROP TABLE manages;
+
+SELECT * FROM manages;  
